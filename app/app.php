477fbaf4c594203208cb2dde5e47948d72e209d5
William Garcia <garcia.rodriguez.william@gmail.com>
2014-08-03 14:01:53 +0200

    Application organization

--- app/app.php
+++ app/app.php
@@ -19,6 +19,6 @@ $app = require __DIR__ . '/routing.php';
 $app = require __DIR__ . '/routing-verbs.php';
 
 // middlewares: authentication, vnd errors
-$app = require __DIR__ . '/middleware.php';
+$app = require __DIR__ . '/middlewares.php';
 
 return $app;
--- app/routing.php
+++ app/routing.php
@@ -2,12 +2,9 @@
 
 $app->get('/', 'Api\Controller\ApiController::indexAction');
 $app->get('/api', 'Api\Controller\ApiController::indexAction');
-
 $app->get('/api/versions', 'Api\Controller\VersionsController::indexAction');
-
 $app->get('/api/errors', 'Api\Controller\ErrorsController::indexAction');
 $app->get('/api/errors/{code}', 'Api\Controller\ErrorsController::detailAction');
-
 $app->get('/api/{version}/customers', 'Api\Controller\CustomersController::indexAction');
 $app->get('/api/{version}/customers/{cid}', 'Api\Controller\CustomersController::detailAction');
 $app->get('/api/{version}/customers/{cid}/licenses', 'Api\Controller\CustomersController::licensesAction');
